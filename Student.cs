20d2d21c520e7a1451d95d6927640b2ff838a5af
NazarYa <58255763+NazarYa@users.noreply.github.com>
2019-11-27 14:04:09 +0200

    Create Student.cs

---
+++ Student.cs
@@ -0,0 +1,10 @@
+using System;
+namespace EFGetStarted
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
